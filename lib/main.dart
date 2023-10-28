5c0b28f65d3522eec80fc90967321c579e8a3443
as1605 <1605.aditya.singh@gmail.com>
2023-10-28 13:01:41 +0530

    https fix certs

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:ocs_tracker/screens/launch.dart';
 import 'package:ocs_tracker/utils/api.dart';
 
+class MyHttpOverrides extends HttpOverrides {
+  // https://stackoverflow.com/a/61312927
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  HttpOverrides.global = MyHttpOverrides();
+
   final api = ApiProvider();
   // await api.credentialProvider.logout();
   runApp(MyApp(api: api));
